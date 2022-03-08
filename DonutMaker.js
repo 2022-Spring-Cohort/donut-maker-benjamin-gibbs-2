a4c0f07aaa7103775042bf7a37bb32cb0f350506
benjamin-gibbs-2 <benjamin.gibbs2@gmail.com>
2022-03-08 15:19:38 -0500

    push

--- DonutMaker.js
+++ DonutMaker.js
@@ -1,25 +1,57 @@
 export default class DonutMaker {
+
+
+
+
     constructor(donutCount, donutMultiplier, autoClicker){
-    this.donutCount = 99;
+    this.donutCount = 0;
     this.donutMultiplier = 0;
+    this.donutMultiplierCost = 10;
     this.autoClicker = 0;
     this.autoClickerCost = 100;
     }
+
+
+
     addToDonutCount() {
         this.donutCount ++;
-//        this.donutCount += Math.power(1.2, this.donutMultiplier);
-//        if (this.donutMultiplier == 0) {
-//        this.donutCount = this.donutCount;
-//        }
-//        else { this.donutCount += Math.power(1.2, this.donutMultiplier); }
+
+        if (this.donutMultiplier == 0) {
+        this.donutCount = this.donutCount;
+
+        }
+        else { this.donutCount += Math.pow(1.2, this.donutMultiplier);
+
+        }
     }
+
+
+
+
+     addToDonutMultiplierCount(){
+        if (this.donutCount >= this.donutMultiplierCost){
+        this.donutMultiplier ++;
+        this.donutCount -= this.donutMultiplierCost;
+        this.donutMultiplierCost = Math.round(this.donutMultiplierCost * 1.1);
+        console.log(this.donutMultiplierCost + "test");
+    }
+        else {
+console.log("You fcker");
+}
+}
+
+
+
     addToAutoClickerCount(){
         if (this.donutCount >= this.autoClickerCost){
         this.autoClicker ++;
         this.donutCount -= this.autoClickerCost;
         this.autoClickerCost = Math.round(this.autoClickerCost * 1.1);
+
     }
-    console.log("Not enough donuts");
+        else {
+console.log("You need more donuts to purchase an AutoClicker");
+}
 }
     activateAutoClickers(){
         for (let i = -0; i < this.autoClicker; i++) {
--- index.js
+++ index.js
@@ -10,18 +10,42 @@ setInterval(function(){
   donutMaker.activateAutoClickers();
   totalDisplay.innerText = "Donut Count: " + Math.round(donutMaker.donutCount);
 },1000);
+
+
 const addDonutBtn = document.querySelector(".donutButton");
 const totalDisplay = document.querySelector(".totalDonutCount")
+const addAutoClickBtn = document.querySelector(".autoClicker");
+const autoDisplay = document.querySelector(".totalAutoClickerCount");
+const addMulitBtn = document.querySelector(".donutMultiplier");
+const mulitDisplay = document.querySelector(".totalDonutMultiplierCount");
+
+
 
 
 addDonutBtn.addEventListener("click", () => {
   donutMaker.addToDonutCount();
   totalDisplay.innerText = "Donut Count: " + Math.round(donutMaker.donutCount);
 })
-const addAutoClickBtn = document.querySelector(".autoClicker");
-const autoDisplay = document.querySelector(".totalAutoClickerCount");
+
+
 addAutoClickBtn.addEventListener("click", () =>{
   donutMaker.addToAutoClickerCount();
   autoDisplay.innerText = "Auto Clicker Count: " + donutMaker.autoClicker;
   totalDisplay.innerText = "Donut Count: " + Math.round(donutMaker.donutCount);
-});
+})
+
+
+
+
+addMulitBtn.addEventListener("click", () =>{
+  donutMaker.addToDonutMultiplierCount();
+  mulitDisplay.innerText = "multipr Count: " + donutMaker.donutMultiplier + " Next Cost: " + donutMaker.donutMultiplierCost;
+  totalDisplay.innerText = "Donut Count: " + Math.round(donutMaker.donutCount);
+})
+
+
+
+
+
+
+;
