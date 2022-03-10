0c4bf55bcf55b7a82e6ff63219aa1253094de137
benjamin-gibbs-2 <benjamin.gibbs2@gmail.com>
2022-03-10 15:31:48 -0500

    Honey Branch Commit 2
    Changed descriptions on front end to align with honey bee related theme.

--- index.js
+++ index.js
@@ -4,28 +4,27 @@ import DonutMaker from "./DonutMaker.js";
 
 
 const donutMaker = new DonutMaker();
-// let donutCount = donutMaker.donutCount;
-// donutMaker.addToDonutCount()
+
 setInterval(function(){
   donutMaker.activateAutoClickers();
   donutMaker.disableAutoClicker();
   totalDisplay.innerText = "Honey jars: " + Math.round(donutMaker.donutCount);
-  //  + "\nDonuts earned per Click: " + (Math.pow(1.2, (donutMaker.donutMultiplier))).toFixed(3);
-  //  "\nDonuts earned per Click: " + Math.pow(1.2, donutMaker.donutMultiplier);
+
   setInterval(function () {
     donutsSecond.innerText =
       "Honey jars earned per Click: " +
       Math.pow(1.2, donutMaker.donutMultiplier).toFixed(3);
-        autoDisplay.innerText = "Auto Clicker Count: " + donutMaker.autoClicker;
+        autoDisplay.innerText = "Worker Bees purchased: " + donutMaker.autoClicker;
         showAutoClickerPrice.innerText =
-          "the next upgrade will cost " +
+          "Next Worker Bee will cost " +
           donutMaker.autoClickerCost +
           " jars of honey";
             mulitDisplay.innerText =
-              "Mutlipliers purchased : " +
-              donutMaker.donutMultiplier +
-              "\nNext Mutliplier Cost: " +
-              donutMaker.donutMultiplierCost; 
+              "Forager group purchased : " +
+              donutMaker.donutMultiplier;
+ 
+
+              multiCount.innerText="Next group of Forager Bees will cost " + donutMaker.donutMultiplierCost + " jars of honey";
   }, 150);
 },1000);
 
@@ -45,7 +44,8 @@ const mulitDisplay = document.querySelector(".totalDonutMultiplierCount");
 const showAutoClickerPrice = document.querySelector(".auto-clicker-display");
 const resetGame = document.querySelector(".resetButton");
 const donutsSecond = document.querySelector(".donutsPerSecond");
-//    showAutoClickerPrice.innerText = autoClicker;
+const multiCount = document.querySelector(".totalMutiCount");
+
 
 resetGame.addEventListener("click", () => {
 donutMaker.resetGame();
@@ -60,8 +60,7 @@ addDonutBtn.addEventListener("click", () => {
 addAutoClickBtn.addEventListener("click", () =>{
 
   donutMaker.addToAutoClickerCount();
-  // autoDisplay.innerText = "Auto Clicker Count: " + donutMaker.autoClicker;
-  // showAutoClickerPrice.innerText = "the next upgrade will cost " + donutMaker.autoClickerCost + " donuts" ;
+
   totalDisplay.innerText = "Honey jars: " + Math.round(donutMaker.donutCount);
 })
 
@@ -70,8 +69,7 @@ addAutoClickBtn.addEventListener("click", () =>{
 
 addMulitBtn.addEventListener("click", () =>{
   donutMaker.addToDonutMultiplierCount();
-  // mulitDisplay.innerText = "Mutlipliers purchased : " + donutMaker.donutMultiplier + "\nNext Mutliplier Cost: " + donutMaker.donutMultiplierCost; 
-  // +  "\nDonuts earned per Click: " + (Math.pow(1.2, (donutMaker.donutMultiplier))).toFixed(3);
+
   totalDisplay.innerText = "Honey jars: " + Math.round(donutMaker.donutCount);
 })
 
